90afabbe8bc961aaf79485ca2802c4aa82219b40
Jaymes Costa <jaaymes@gmail.com>
2022-10-31 22:20:09 -0300

    corrigindo erro build

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,13 +1,9 @@
-import { useForm } from 'react-hook-form';
-
 import Head from 'next/head';
 
 import { useProduct } from '@/hooks/useProduct';
 
-import FormProvider from '@/components/hook-form';
 import { ShopProductList, ShopProductSearch } from '@/components/shop';
 
-import { IProductFilter } from '@/@types/product';
 import LayoutShop from '@/layouts/LayoutShop';
 import { Typography, Stack, Box } from '@mui/material';
 
@@ -17,59 +13,33 @@ EcommerceShopPage.getLayout = (page: React.ReactElement) => (
 
 export default function EcommerceShopPage() {
   const { product } = useProduct()
-
-  const defaultValues = {
-    gender: [],
-    category: 'All',
-    colors: [],
-    priceRange: [0, 200],
-    rating: '',
-    sortBy: 'featured',
-  };
-
-  const methods = useForm<IProductFilter>({
-    defaultValues,
-  });
-
-  const {
-    formState: { dirtyFields },
-  } = methods;
-
-  const isDefault =
-    (!dirtyFields.optional &&
-      !dirtyFields.category &&
-      !dirtyFields.price) || false;
-
   return (
     <>
       <Head>
         <title> Agile Motors | Store</title>
       </Head>
+      <Box sx={{ p: 2 }}>
+        <Stack
+          spacing={2}
+          direction={{ xs: 'column', sm: 'row' }}
+          alignItems={{ sm: 'center' }}
+          justifyContent="space-between"
+          sx={{ mb: 2 }}
+        >
+          <ShopProductSearch />
+        </Stack>
 
-      <FormProvider methods={methods}>
-        <Box sx={{ p: 2 }}>
-          <Stack
-            spacing={2}
-            direction={{ xs: 'column', sm: 'row' }}
-            alignItems={{ sm: 'center' }}
-            justifyContent="space-between"
-            sx={{ mb: 2 }}
-          >
-            <ShopProductSearch />
-          </Stack>
-
-          <Stack sx={{ mb: 3 }}>
-            {!product?.length && (
-              <>
-                <Typography variant="body1" gutterBottom alignSelf={"center"}>
-                  &nbsp;Nenhum Produto
-                </Typography>
-              </>
-            )}
-          </Stack>
-          <ShopProductList products={product} loading={!product?.length && isDefault} />
-        </Box>
-      </FormProvider>
+        <Stack sx={{ mb: 3 }}>
+          {!product?.length && (
+            <>
+              <Typography variant="body1" gutterBottom alignSelf={"center"}>
+                &nbsp;Nenhum Produto
+              </Typography>
+            </>
+          )}
+        </Stack>
+        <ShopProductList products={product} loading={!product?.length} />
+      </Box>
     </>
   );
 }
