54b19cdb575d68ead79290d7714f180adabd0a3e
Jaymes Costa <jaaymes@gmail.com>
2022-11-28 20:36:20 -0300

    ajustando redirecionamentos

--- src/components/shop/ShopProductCard.tsx
+++ src/components/shop/ShopProductCard.tsx
@@ -105,7 +105,7 @@ export default function ShopProductCard({ product }: Props) {
               `/shop/veiculo.html?id=${product?.idVeiculo}&idFranqueado=${product?.idFranqueado}`
           } passHref>
             {/* @ts-ignore */}
-            <Button target="_blank" rel="noopener noreferrer" > + Detalhes</Button>
+            <Button target="_blank" rel="noopener noreferrer" variant='contained' > + Detalhes</Button>
           </Link>
 
 
